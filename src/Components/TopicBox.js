c6bb74d0414f73d32b86dcb9f792da7f7f37d63f
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-24 10:32:28 +0530

    add topic box js

--- src/Components/TopicBox.js
+++ src/Components/TopicBox.js
@@ -5,7 +5,7 @@ export default function TopicBox() {
   return (
     <div>
       <div className="topicBox">
-        <span>This is the Topic Box</span>
+        <span className='text'>This is the Topic Box</span>
       </div>
     </div>
   )
