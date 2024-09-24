e3ef99fa94c0d6709037d75d455181035d4c357c
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-24 10:32:03 +0530

    add content js

---
+++ src/Components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Content.css'
+
+export default function Content() {
+  return (
+    <div className='paragraph'>
+      <p>Knowing how to write a paragraph is incredibly important. It’s a basic aspect of writing, and it is 
+        something that everyone should know how to do. There is a specific structure that you have to follow when
+         you’re writing a paragraph. This structure helps make it easier for the reader to understand what is 
+         going on. Through writing good paragraphs, a person can communicate a lot better through their writing.
+
+        When you want to write a paragraph, most of the time you should start off by coming up with an idea.
+         After you have your idea or topic, you can start thinking about different things you can do to expand
+          upon that idea. You should only finish the paragraph when you’ve finished covering everything you want
+           about that idea.</p>
+    </div>
+  )
+}
