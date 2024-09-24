9fc4478fdd4c02f3af61b4907b35fea060a7c551
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-24 14:37:15 +0530

    Route pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
