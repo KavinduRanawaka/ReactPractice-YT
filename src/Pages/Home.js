38d7abcaab116f79ecad1207951757e0623b0a38
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-24 14:38:03 +0530

    Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import TopicBox from '../Components/TopicBox'
 import Content from '../Components/Content'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>This is Home Page</h1>
 
       <TopicBox food="Rice" price='150'>
         <span>I Love Rice</span>
@@ -16,6 +17,14 @@ export default function Home() {
       </TopicBox>
 
       <TopicBox food="Kottu" price='100'/>
+
+      <button>
+      <Link to="/About">Go to AboutPage</Link>
+      </button>
+
+      <button>
+      <Link to="/Contact">Go to ContactPage</Link>
+      </button>
       
     </div>
   )
