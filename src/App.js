3878e58fe757c0360dbce6c7b6f8ca9dc9fb2944
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-25 11:31:43 +0530

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import EventHandler from './Pages/EventHandler';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <Router>
-      <div>
+    <div>
+
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
+            <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link href="/EventHandler">EventHandler</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
+          <Route path="/EventHandler" element={<EventHandler/>}/>
         </Routes>
-      </div>
-    </Router>
+    </BrowserRouter>
+    </div>
   );
 }
 
