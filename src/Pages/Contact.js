33144747a6801263fd422a55e87ec555b22d01ec
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-25 08:38:42 +0530

    use state

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function Contact() {
-  let count=0;
+  const[count,setCount]=useState(0)
+  // let count=0;
+
+  const increment=()=>{
+    setCount(count+1);
+  }
+  const decrement=()=>{
+    setCount(count-1);
+  }
 
   return (
     <div>
      
       <span>My Counter</span>
       <p>The Count is {count}</p>
-      <button>-</button>
-      <button>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+
     </div>
   )
 }
